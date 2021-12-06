bfd33e5493462a4a9e84d1b28015112b9ec9fd8d
eren kiraz <erengkiraz@gmail.com>
2021-12-06 16:22:31 +0300

    group crud added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TokenMiddleware } from 'libs/middlewares/token.middleware';
 import environment from 'tools/environment/environment';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GroupModule } from './group/group.module';
 import { LoginModule } from './login/login.module';
 import { TotalModule } from './total/total.module';
 import { UserModule } from './user/user.module';
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
   imports: [UserModule,
     TotalModule,
     LoginModule,
+    GroupModule,
     MongooseModule.forRoot(environment.mongoUrl)
   ],
   controllers: [AppController],
