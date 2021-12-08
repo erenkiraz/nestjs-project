5f0cbc578fab79066f791ca7d2b76c8b199430b8
eren kiraz <erengkiraz@gmail.com>
2021-12-08 10:35:33 +0300

    swager added.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AllExceptionFilter } from 'libs/filters/all-exception.filter';
 import { AppModule } from './app.module';
 
@@ -9,6 +10,16 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+  .setTitle('Udemy Course API endpoints')
+  .setDescription('Udemy Course API endpoints')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
